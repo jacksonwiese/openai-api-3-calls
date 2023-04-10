c6d8c14225c99402f8972453f98772a6aa0a4466
jacksonwiese <jacksonwiese1@gmail.com>
2023-04-09 22:56:39 -0500

    Dall-e, prompt completion, and sentiment analysis using ChatGPT API

--- openai-3-api-call/ContentView.swift
+++ openai-3-api-call/ContentView.swift
@@ -5,17 +5,29 @@
 //  Created by Jackson Wiese on 4/9/23.
 //
 
+
+import OpenAIKit
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView {
+            DallESearch()
+                .tabItem {
+                    Image(systemName: "photo")
+                    Text("Image Gen")
+                }
+            CompleteSentence()
+                .tabItem {
+                    Image(systemName: "text.redaction")
+                    Text("Sentence")
+                }
+            SentimentAnalysis()
+                .tabItem{
+                    Image(systemName: "face.smiling.inverse")
+                    Text("Sentiment")
+                }
+        }//end of TabView
     }
 }
 
