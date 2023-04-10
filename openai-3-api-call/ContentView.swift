e4753e596aa9ec163515de988b58f937374f5130
jacksonwiese <jacksonwiese1@gmail.com>
2023-04-09 21:45:03 -0500

    Initial Commit

---
+++ openai-3-api-call/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  openai-3-api-call
+//
+//  Created by Jackson Wiese on 4/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ openai-3-api-call/openai_3_api_callApp.swift
@@ -0,0 +1,17 @@
+//
+//  openai_3_api_callApp.swift
+//  openai-3-api-call
+//
+//  Created by Jackson Wiese on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct openai_3_api_callApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
